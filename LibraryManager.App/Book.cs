174abc9551ed61df01aaed01bf4f0c5ba7508851
Neekzen <neekzen.62@gmail.com>
2024-12-16 10:37:05 +0100

    etape 2: Commencer son programme

---
+++ LibraryManager.App/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.App
+{
+    public class Book
+    {
+        public string? name;
+        public string? type;
+
+        public Book(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+
+        public string DisplayBook()
+        {
+            return "Nom du Livre: " + this.name + " | " + this.type;
+        }
+    }
+}
--- LibraryManager.App/Program.cs
+++ LibraryManager.App/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LibraryManager.App;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Book> books = new List<Book>();
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Livre " + i);
+            Console.WriteLine("Entrez le nom du livre :");
+            books.Add(new Book(Console.ReadLine(), "Aventure"));
+        }
+
+        foreach(Book book in books)
+            Console.WriteLine(book.DisplayBook());
+    }
+}
