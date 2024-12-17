d4b9a665c55dc231f3033c9c665266ac90ae7550
Neekzen <neekzen.62@gmail.com>
2024-12-17 15:05:55 +0100

    etape 6 : EntityFramework

--- LibraryManager.App/Program.cs
+++ LibraryManager.App/Program.cs
@@ -1,36 +1,37 @@
 ﻿using BusinessObjects.App;
 using BusinessObjects.Entity;
 using BusinessObjects.Enum;
+using DataAccessLayer.Contexts;
 using DataAccessLayer.Repository;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Services.Services;
-using System.Data;
+using Microsoft.EntityFrameworkCore;
 
 public class Program
 {
 
-    private static IHost CreateHostBuilder(List<Book> books, List<Author> authors, List<Library> libraries)
+    private static IHost CreateHostBuilder()
     {
         return Host.CreateDefaultBuilder()
-            .ConfigureServices(services =>
+            .ConfigureServices((context, services) =>
             {
-                // Register datas
-                services.AddSingleton(books);
-                services.AddSingleton(authors);
-                services.AddSingleton(libraries);
+                // Enregistrer le DbContext
+                services.AddDbContext<LibraryContext>(options =>
+                    options.UseSqlite("Data Source=C:\\Users\\Asus\\source\\repos\\LibraryManager\\LibraryManager.App\\ressources\\library.db"));
+                // Mettez le chemin absolu vers la base SQLite
 
-                // Register All Repositories
-                services.AddTransient<IRepository<Book>, BookRepository>();
-                services.AddTransient<IRepository<Author>, AuthorRepository>();
-                services.AddTransient<IRepository<Library>, LibraryRepository>();
+                // Enregistrer GenericRepository pour chaque type d'entité
+                services.AddTransient<IGenericRepository<Book>, GenericRepository<Book>>();
+                services.AddTransient<IGenericRepository<Author>, GenericRepository<Author>>();
+                services.AddTransient<IGenericRepository<Library>, GenericRepository<Library>>();
 
-                // Register RepositoryManager
+                // Enregistrer RepositoryManager modifié pour utiliser IGenericRepository<T>
                 services.AddTransient<IRepositoryManager, RepositoryManager>(serviceProvider =>
                 {
-                    var bookRepo = serviceProvider.GetRequiredService<IRepository<Book>>();
-                    var authorRepo = serviceProvider.GetRequiredService<IRepository<Author>>();
-                    var libraryRepo = serviceProvider.GetRequiredService<IRepository<Library>>();
+                    var bookRepo = serviceProvider.GetRequiredService<IGenericRepository<Book>>();
+                    var authorRepo = serviceProvider.GetRequiredService<IGenericRepository<Author>>();
+                    var libraryRepo = serviceProvider.GetRequiredService<IGenericRepository<Library>>();
 
                     var manager = new RepositoryManager();
                     manager.RegisterRepository(bookRepo);
@@ -40,7 +41,7 @@ public class Program
                     return manager;
                 });
 
-                // Register BookServices
+                // Enregistrer BookServices
                 services.AddTransient<IBookServices, BookServices>();
             })
             .Build();
@@ -48,47 +49,7 @@ public class Program
 
     private static void Main(string[] args)
     {
-        List<Author> authors = new List<Author>
-        {
-            new Author(1, "Alexandre", "Dumas"),
-            new Author(2, "Remy", "Synave"),
-            new Author(3, "Dany", "Capitaine"),
-            new Author(4, "Severine", "Lettrez"),
-            new Author(5, "George", "Remi"),
-            new Author(6, "Maxime", "Chattam")
-        };
-
-        List<Book> books = new List<Book>
-        {
-            new Book(1, "Le conte de Monte Cristo", 900, TypeBook.Aventure, 10, authors[0]),
-            new Book(2, "Les trois mousquetaires", 300, TypeBook.Aventure, 9, authors[0]),
-            new Book(3, "Apprendre le Java mais pas sur l'île de Java", 900, TypeBook.Enseignement, 10, authors[1]),
-            new Book(4, "Le RC Lens, un club pas comme les autres", 900, TypeBook.Histoire, 10, authors[3]),
-            new Book(5, "La RGPD, une protection contre l'injustice", 900, TypeBook.Juridique, 10, authors[0]),
-            new Book(6, "Les aventures de Tintin : Le temple du Soleil", 300, TypeBook.Aventure, 9, authors[4]),
-            new Book(7, "Le Signal", 1800, TypeBook.Horreur, 10, authors[5])
-        };
-
-        List<Library> libraries = new List<Library>
-        {
-            new Library(1, "Bibliothèque Roubaix", "44 Av. Jean Lebas, 59100 Roubaix"),
-            new Library(2, "Médiathèque Calais", "16 Rue du Pont Lottin, 62100 Calais")
-        };
-
-        // Set books in libraries
-        foreach (var library in libraries)
-        {
-            if (library.Id == 1)
-            {
-                library.Books = books.Where(b => b.Type == TypeBook.Aventure || b.Type == TypeBook.Horreur).ToList();
-            }
-            else if (library.Id == 2)
-            {
-                library.Books = books.Where(b => b.Type != TypeBook.Aventure && b.Type != TypeBook.Horreur).ToList();
-            }
-        }
-
-        var host = CreateHostBuilder(books, authors, libraries);
+        var host = CreateHostBuilder();
 
         var repositoryManager = host.Services.GetRequiredService<IRepositoryManager>();
         var bookServices = host.Services.GetRequiredService<IBookServices>();
